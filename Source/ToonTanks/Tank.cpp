0f280a5df9ce858618ac362d05045a182499ee7f
Katsanakis Georgios <geo.elgeo@gmail.com>
2022-06-03 09:31:32 +0200

    added tank rotation

--- Source/ToonTanks/Tank.cpp
+++ Source/ToonTanks/Tank.cpp
@@ -7,26 +7,32 @@
 
 ATank::ATank()
 {
-  SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("Spring Arm"));
-  SpringArm->SetupAttachment(RootComponent);
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("Spring Arm"));
+	SpringArm->SetupAttachment(RootComponent);
 
-  Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
-  Camera->SetupAttachment(SpringArm);
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(SpringArm);
 }
 
 void ATank::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
-  Super::SetupPlayerInputComponent(PlayerInputComponent);
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
-  PlayerInputComponent->BindAxis(TEXT("MoveForward"), this, &ATank::Move);
-
-  
+	PlayerInputComponent->BindAxis(TEXT("MoveForward"), this, &ATank::Move);
+	PlayerInputComponent->BindAxis(TEXT("Turn"), this, &ATank::Turn);
 }
 
 
 void ATank::Move(float Value)
-{ 
-  FVector DeltaLocation = FVector::ZeroVector;
-  DeltaLocation.X = Value * Speed *UGameplayStatics::GetWorldDeltaSeconds(this);
-  AddActorLocalOffset(DeltaLocation);
+{
+	FVector DeltaLocation = FVector::ZeroVector;
+	DeltaLocation.X = Value * Speed * UGameplayStatics::GetWorldDeltaSeconds(this);
+	AddActorLocalOffset(DeltaLocation, true);
+}
+
+void ATank::Turn(float Value)
+{
+	FRotator DeltaRotation = FRotator::ZeroRotator;
+	DeltaRotation.Yaw = Value * TurnRate * UGameplayStatics::GetWorldDeltaSeconds(this);
+	AddActorLocalRotation(DeltaRotation,true);
 }
