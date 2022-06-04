8aa068a2edc1c55900a6eab66c18bb8f045790c7
Katsanakis Georgios <geo.elgeo@gmail.com>
2022-06-04 17:55:02 +0200

    added timehandler and refactored some code

--- Source/ToonTanks/Tower.cpp
+++ Source/ToonTanks/Tower.cpp
@@ -9,13 +9,9 @@ void ATower::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	if (Tank)
+	if (InFireRange())
 	{
-		float Distance = FVector::Dist(GetActorLocation(), Tank->GetActorLocation());
-		if (Distance <= FireRange)
-		{
-			RotateTurret(Tank->GetActorLocation());
-		}
+		RotateTurret(Tank->GetActorLocation());		
 	}
 }
 
@@ -24,4 +20,28 @@ void ATower::BeginPlay()
 	Super::BeginPlay();
 
 	Tank = Cast<ATank>(UGameplayStatics::GetPlayerPawn(this, 0));
+
+	GetWorldTimerManager().SetTimer(FireRateTimerHandle, this, &ATower::CheckFireCondition, FireRate, true);
+}
+
+void ATower::CheckFireCondition()
+{
+	if (InFireRange())
+	{
+		Fire();
+	}
+}
+
+bool ATower::InFireRange()
+{
+	if (Tank)
+	{
+		float Distance = FVector::Dist(GetActorLocation(), Tank->GetActorLocation());
+
+		if (Distance <= FireRange)
+		{
+			return true;
+		}
+	}
+	return false;
 }
--- Source/ToonTanks/Tower.h
+++ Source/ToonTanks/Tower.h
@@ -25,4 +25,10 @@ private:
 
 	UPROPERTY(EditDefaultsOnly, Category="Compat")
 	float FireRange = 300.f;
+
+	FTimerHandle FireRateTimerHandle;
+	float FireRate = 2.f;
+	void CheckFireCondition();
+
+	bool InFireRange();
 };
